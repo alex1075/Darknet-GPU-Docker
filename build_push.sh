1a861da111243df1e86b6746abd403669d58ef15
Alexander Hunt <alexander.hunt@ed.ac.uk>
2022-03-29 14:24:13 +0100

    On branch main  Your branch is up-to-date with 'origin/main'.  Changes to be committed: 	modified:   build_push.sh

--- build_push.sh
+++ build_push.sh
@@ -2,7 +2,10 @@
 echo "Building and pushing to Dockerhub"
 echo "Building image"
 docker build -t alex1075/phd:darknetty . --platform linux/amd64
-eho "Build complete"
+echo " "
+echo " "
+echo " "
+ehco "Build complete"
 echo "Pushing to Dockerhub"
 docker push alex1075/phd:darknetty
 echo "Push complete"
